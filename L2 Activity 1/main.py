f30cf9c962dae92ab95f4ab5d3e7b78cda57c5db
asif1416 <ashrafulasif260@gmail.com>
2024-08-19 18:39:39 +0600

    lesson 1 Activity 1 completed

---
+++ L2 Activity 1/main.py
@@ -0,0 +1,15 @@
+#print a sentence 
+print('Welcome to the world of programming!')
+
+# print a number
+print(4)
+
+# next line after print
+print('Hello python \n')
+
+# pass multiple arguments 
+print("Hello", 5)
+
+#End argument of print statement
+print("Welcome to ", end="*")
+
