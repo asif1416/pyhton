81c9808fab55b28167374926b6cb47bc06cc580c
asif1416 <ashrafulasif260@gmail.com>
2024-08-20 14:22:03 +0600

    Lessonn2 Activity 2-3 Added

---
+++ L2 Activity 2/main.py
@@ -0,0 +1,14 @@
+#Creating different variables
+x = 5
+y = "Asif"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input
+name = input("ENter your name: ")
+
+print("\nHello", name, "\nWelcome to Codingal")
+
---
+++ L2 Activity 3/main.py
@@ -0,0 +1,5 @@
+import keyword
+
+#printing alll the keywords
+print("All the keywords are.. \n")
+print(keyword.kwlist)
