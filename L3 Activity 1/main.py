15e5e3afea0b8a69264eefefc1e9d8b716f4b318
asif1416 <ashrafulasif260@gmail.com>
2024-08-20 14:42:38 +0600

    Lesson 3 added

---
+++ L3 Activity 1/main.py
@@ -0,0 +1,12 @@
+#Checking the datatypes in python
+a=10
+print("Type of a: ",type(a))
+
+b=10.5
+print("Type of a: ",type(b))
+
+c="Coding"
+print("Type of a: ",type(c))
+
+d=True
+print("Type of a: ",type(d))
---
+++ L3 Activity 2/main.py
@@ -0,0 +1,28 @@
+#Assigning different variables
+name="Asif"
+age = 24
+is_student = True
+weight = 70
+
+#Printing Different variables and their Data type
+print("Name: ",name)
+print("Data type of Name is: ",type(name))
+
+print("Age: ",age)
+print("Data type of age is: ",type(age))
+
+print("is_student: ",is_student)
+print("Data type of is_student is: ",type(is_student))
+
+print("Weight: ",weight)
+print("Data type of weight is: ",type(weight))
+
+#Type casting to convert the datatype of variables 
+print("\n After Type Casting")
+age = str(age)
+print(age)
+print("Data type of age is: ",type(age))
+
+weight = int(weight)
+print(weight)
+print("Data type of weight is: ",type(weight))
